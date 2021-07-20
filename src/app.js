19e105b7d25f0c1e89fb58fb2bca2ad1baa6122d
dafnemus <dafnemus@gmail.com>
2021-07-20 14:37:32 -0300

    agrego timer y finalizo la app

--- src/app.js
+++ src/app.js
@@ -4,42 +4,62 @@ const buttonClick = document.getElementById('buttonClick');
 const progress = document.getElementById('progress');
 const maxClicks = 5;
 let initialClick = 0;
-const time = 30;
+let time = 30;
+let timerId;
 
-const countClicks = () => {
-  return (initialClick += 1);
+const countClicks = () => initialClick += 1
+
+const discountTime = () => {
+  if (time === 0 && initialClick < maxClicks) {
+    perder();
+  }
+  time--;
+  timer.innerHTML = `00:${time}`;
 };
 
 const play = () => {
   buttonClick.removeAttribute('disabled');
-  discountTime()
-  timer.innerHTML = `00:${time}`
+  timerId = setInterval(discountTime, 1000)
 };
 
 const pause = () => {
   buttonClick.setAttribute('disabled', true);
-};
-
-
-const discountTime = () => {
-  return (time -= 1);
+  timerId = clearInterval(timerId)
 };
 
 const clickMe = () => {
-  const move_y = (Math.random() * 450) - 1;
-  const move_x = (Math.random() * 800) - 1;
-  buttonClick.style.transform = `translate(${move_x}px, ${move_y}px)`;
-  progress.style.background = `linear-gradient(
-        to right,
-        var(--green) 0%,
-        var(--green) calc(20% * ${initialClick + 1}),
-        #fff 20%,
-        #fff 100%
-      )`;
   if (initialClick === maxClicks) {
-    window.alert('Ganaste');
-    document.location.reload();
+    ganar();
   }
+  console.log(initialClick)
+  getProgress();
   countClicks();
+  moveButton();
   clicks.innerHTML = `${initialClick}/${maxClicks} clicked`;
+};
+
+const ganar = () => {
+  alert('Ganaste');
+  document.location.reload();
+};
+
+const perder = () => {
+  alert('Perdiste');
+  document.location.reload();
+};
+
+const moveButton = () => {
+  const move_y = Math.random() * 400;
+  const move_x = Math.random() * 400;
+  buttonClick.style.transform = `translate(${move_x}%, ${move_y}%)`;
+};
+
+const getProgress = () => {
+  progress.style.background = `linear-gradient(
+    to right,
+    var(--green) 0%,
+    var(--green) calc(20% * ${initialClick + 1}),
+    #fff 20%,
+    #fff 100%
+  )`;
 };
