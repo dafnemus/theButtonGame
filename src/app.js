e2e423b192654d4967a7acfda887d4ebe07827ac
dafnemus <dafnemus@gmail.com>
2021-07-19 21:31:23 -0300

    defino tamaño para el boton click me

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const clicks = document.getElementById('clicks');
 const timer = document.getElementById('timer');
 const buttonClick = document.getElementById('buttonClick');
-const progress = document.getElementById('progress')
+const progress = document.getElementById('progress');
 const maxClicks = 5;
 let initialClick = 0;
+const time = 30;
 
 const countClicks = () => {
   return (initialClick += 1);
@@ -11,30 +12,32 @@ const countClicks = () => {
 
 const play = () => {
   buttonClick.removeAttribute('disabled');
-  
+  discountTime(time)
 };
 
 const pause = () => {
-    buttonClick.setAttribute('disabled', true)
-}
+  buttonClick.setAttribute('disabled', true);
+};
+
+const discountTime = (time) => {
+  return (time -= 1);
+};
 
 const clickMe = () => {
-    const move_y = Math.random() * 350
-    const move_x = Math.random() * 400
-    buttonClick.style.transform = `translate(${move_x}px, ${move_y}px)`
-    progress.style.background = `linear-gradient(
+  const move_y = (Math.random() * 450) - 1;
+  const move_x = (Math.random() * 800) - 1;
+  buttonClick.style.transform = `translate(${move_x}px, ${move_y}px)`;
+  progress.style.background = `linear-gradient(
         to right,
         var(--green) 0%,
         var(--green) calc(20% * ${initialClick + 1}),
         #fff 20%,
         #fff 100%
-      )`
-    if (initialClick === maxClicks) {
-      window.alert('Ganaste');
-      document.location.reload();
-    }
-    countClicks();
-    clicks.innerHTML = `${initialClick}/${maxClicks} clicked`;
+      )`;
+  if (initialClick === maxClicks) {
+    window.alert('Ganaste');
+    document.location.reload();
+  }
+  countClicks();
+  clicks.innerHTML = `${initialClick}/${maxClicks} clicked`;
 };
-
-
